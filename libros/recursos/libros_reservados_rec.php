9e533b35c9334c1148f53efcb8a01a9946b6c4a2
David12tl <168271073+David12tl@users.noreply.github.com>
2025-04-02 15:41:09 -0600

    Add files via upload

---
+++ libros/recursos/libros_reservados_rec.php
@@ -0,0 +1,194 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <title>LIBROS RESERVADOS</title>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="Shortcut Icon" type="image/x-icon" href="../assets/icons/book.ico" />
+    <script src="../../js/sweet-alert.min.js"></script>
+    <link rel="stylesheet" href="../../css/sweet-alert.css">
+    <link rel="stylesheet" href="../../css/material-design-iconic-font.min.css">
+    <link rel="stylesheet" href="../../css/normalize.css">
+    <link rel="stylesheet" href="../../css/bootstrap.min.css">
+    <link rel="stylesheet" href="../../css/jquery.mCustomScrollbar.css">
+    <link rel="stylesheet" href="../../css/home.css">
+    <script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
+    <script>window.jQuery || document.write('<script src="js/jquery-1.11.2.min.js"><\/script>')</script>
+    <script src="../../js/modernizr.js"></script>
+    <script src="../../js/bootstrap.min.js"></script>
+    <script src="../../js/jquery.mCustomScrollbar.concat.min.js"></script>
+    <script src="../../js/main.js"></script>
+</head>
+<body>
+<?php
+    include("../../config/config.php");
+    include("../../acciones/accion_disponible.php");
+
+    $Libros = obtenerLibros_reservados($conexion);
+    $totalLibros = $Libros->num_rows;
+    ?>
+
+    <div class="navbar-lateral full-reset">
+        <div class="visible-xs font-movile-menu mobile-menu-button"></div>
+        <div class="full-reset container-menu-movile nav-lateral-scroll">
+            <div class="logo full-reset all-tittles">
+                <i class="visible-xs zmdi zmdi-close pull-left mobile-menu-button" style="line-height: 55px; cursor: pointer; padding: 0 10px; margin-left: 7px;"></i> 
+                
+            </div>
+            <div class="nav-lateral-divider full-reset"></div>
+            <div class="full-reset" style="padding: 10px 0; color:#fff;">
+                <figure>
+                    <img src="../../assets/img/logo.png" alt="Biblioteca" class="img-responsive center-box" style="width:100%; margin-top: -3.6%">
+                </figure>
+                <h1 class="text-center" style="padding-top: 15px;">S C I</h1>
+            </div>
+            <div class="nav-lateral-divider full-reset"></div>
+            <div class="full-reset nav-lateral-list-menu">
+            <ul class="list-unstyled">
+                <li><a href="../../home_rec.php"><i class="zmdi zmdi-home zmdi-hc-fw"></i>&nbsp;&nbsp; Inicio</a></li>
+                    <li>
+                        <div class="dropdown-menu-button"><i class="zmdi zmdi-assignment-o zmdi-hc-fw"></i>&nbsp;&nbsp; Libros y catálogo <i class="zmdi zmdi-chevron-down pull-right zmdi-hc-fw icon-sub-menu"></i></div>
+                        <ul class="list-unstyled">
+                            <li><a href="../../autor/autor_rec.php"><i class="zmdi zmdi-accounts zmdi-hc-fw"></i>&nbsp;&nbsp; Autor</a></li>
+                            <li><a href="../../catalogo/libro_añadir_rec.php"><i class="zmdi zmdi-book zmdi-hc-fw"></i>&nbsp;&nbsp; Nuevo libro</a></li>
+                            <li><a href="../../catalogo/rec_catalogo.php"><i class="zmdi zmdi-bookmark-outline zmdi-hc-fw"></i>&nbsp;&nbsp; Catálogo</a></li>
+                        </ul>
+                    </li>
+                    <li>
+                        <div class="dropdown-menu-button"><i class="zmdi zmdi-alarm zmdi-hc-fw"></i>&nbsp;&nbsp; Préstamos y reservaciones <i class="zmdi zmdi-chevron-down pull-right zmdi-hc-fw icon-sub-menu"></i></div>
+                        <ul class="list-unstyled">
+                            <li><a href="libros_disponible_rec.php"><i class="zmdi zmdi-calendar zmdi-hc-fw"></i>&nbsp;&nbsp;Libros disponibles</a></li>
+                            <li>
+                                <a href="libros_ocupados_rec.php"><i class="zmdi zmdi-time-restore zmdi-hc-fw"></i>&nbsp;&nbsp;Libros Ocupados<span class="label label-danger pull-right label-mhover"></span></a>
+                            </li>
+                            <li>
+                                <a href="libros_reservados_rec.php"><i class="zmdi zmdi-timer zmdi-hc-fw"></i>&nbsp;&nbsp; Libros Reservaciones <span class="label label-danger pull-right label-mhover"></span></a>
+                            </li>
+                        </ul>
+                    </li>
+                    <li><a href="../../reportes/report_rec.php"><i class="zmdi zmdi-trending-up zmdi-hc-fw"></i>&nbsp;&nbsp; Reportes y estadísticas</a></li>
+                    <li><a href="advancesettings.html"><i class="zmdi zmdi-wrench zmdi-hc-fw"></i>&nbsp;&nbsp; Configuraciones avanzadas</a></li>
+                </ul>
+            </div>
+        </div>
+    </div>
+    <div class="content-page-container full-reset custom-scroll-containers">
+        <nav class="navbar-user-top full-reset">
+            <ul class="list-unstyled full-reset">
+                <figure>
+                   <img src="../../assets/img/user04.png" alt="user-picture" class="img-responsive img-circle center-box">
+                </figure>
+                <li style="color:#fff; cursor:default;">
+                    <span class="all-tittles">Administrador de Recursos</span>
+                </li>
+                <li  class="tooltips-general exit-system-button" data-href="../../Index.php" data-placement="bottom" title="Salir del sistema">
+                    <i class="zmdi zmdi-power"></i>
+                </li>
+                <li  data-placement="bottom" title="Buscar libro">
+                    <a href="../../buscador/buscar_libro_rec.php"> <h2><i class="zmdi zmdi-search"></i></h2></a>
+                </li>
+                <li  class="tooltips-general btn-help" data-placement="bottom" title="Ayuda">
+                    <i class="zmdi zmdi-help-outline zmdi-hc-fw"></i>
+                </li>
+                <li class="mobile-menu-button visible-xs" style="float: left !important;">
+                    <i class="zmdi zmdi-menu"></i>
+                </li>
+                <li class="desktop-menu-button hidden-xs" style="float: left !important;">
+                    <i class="zmdi zmdi-swap"></i>
+                </li>
+            </ul>
+        </nav>
+        <div class="container">
+            <div class="page-header">
+              <h1 class="all-tittles">Sistema de control de inventario <small>LISTA DE LIBROS RESERVADOS</small></h1>
+            </div>      
+        </div>
+        <div class="container-fluid"  style="margin: 50px 0;">
+            <div class="row">
+                <div class="col-xs-12 col-sm-4 col-md-3">
+                <img src="../../assets/img/clock.png" alt="pdf" class="img-responsive center-box" style="max-width: 110px;">
+                </div>
+                <div class="col-xs-12 col-sm-8 col-md-8 text-justify lead">
+                    <p>Bienvenido a la sección Lista de libros, en este apartado podrás visualizar los libros es estado Reservado</p>
+                </div>
+            </div>
+        </div>
+        <div class="table-responsive">
+    <table class="table table-hover" id="table_Libros">
+        <thead>
+            <tr>
+                <th scope="col">ID</th>
+                <th scope="col">Nombre</th>
+                <th scope="col">Autor</th>
+                <th scope="col">Edicion</th>
+                <th scope="col">Genero</th>
+                <th scope="col">fecha_publicacion</th>
+                <th scope="col">Espacio</th>
+                <th scope="col">Portada</th>
+            </tr>
+        </thead>
+        <tbody>
+            <?php
+            foreach ($Libros as $Libro) { ?>
+                <tr id="Libros_<?php echo $Libro['id_libros']; ?>">
+                    <th scope='row'><?php echo $Libro['id_libros']; ?></th>
+                    <td><?php echo $Libro['nombre']; ?></td>
+                    <td><?php echo $Libro['autor']; ?></td>
+                    <td><?php echo $Libro['edicion']; ?></td>
+                    <td><?php echo $Libro['genero']; ?></td>
+                    <td><?php echo $Libro['fecha_publicacion']; ?></td>
+                    <td><?php echo $Libro['espacio']; ?></td>
+                    <td>
+                        <?php
+                        $Portada = $Libro['Portada'];
+                        if ($Portada == '') {
+                            $Portada = '../../assets/imgs/sin-foto.jpg';
+                        } else {
+                            $Portada = "../../acciones/fotos_portadas/" . $Portada;
+                        }
+                        ?>
+                        <img src="<?php echo $Portada; ?>" alt="<?php echo $Libro['nombre']; ?>" style="width: 100px; height: auto;">
+                    </td>
+                </tr>
+            <?php } ?>
+        </tbody>
+    </table>
+</div>
+        <div class="modal fade" tabindex="-1" role="dialog" id="ModalHelp">
+          <div class="modal-dialog modal-lg">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+                    <h4 class="modal-title text-center all-tittles">ayuda del sistema</h4>
+                </div>
+                <div class="modal-body">
+                Si tienes una duda específica, nuestro equipo de soporte está listo para ayudarte. Puedes contactarnos a través de Lexis@gmail.com.mx para obtener asistencia personalizada.
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-primary" data-dismiss="modal"><i class="zmdi zmdi-thumb-up"></i> &nbsp; De acuerdo</button>
+                </div>
+            </div>
+          </div>
+        </div>
+        <footer class="footer full-reset">
+            <div class="container-fluid">
+                <div class="row">
+                    <div class="col-xs-12 col-sm-6">
+                        <h4 class="all-tittles">Acerca de</h4>
+                        <p>
+                        Esta biblioteca digital es un proyecto destinado a preservar y difundir el patrimonio cultural y académico. Los contenidos están disponibles para fines educativos y de investigación.
+                        </p>
+                    </div>
+                    <div class="col-xs-12 col-sm-6">
+                        <h4 class="all-tittles">Desarrollador</h4>
+                        <ul class="list-unstyled">
+                            <li><i class="zmdi zmdi-check zmdi-hc-fw"></i>&nbsp; LEXIS <i class="zmdi zmdi-facebook zmdi-hc-fw footer-social"></i><i class="zmdi zmdi-twitter zmdi-hc-fw footer-social"></i></li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+            <div class="footer-copyright full-reset all-tittles">© 2025</div>
+        </footer>
+    </div>
+</body>
+</html>
